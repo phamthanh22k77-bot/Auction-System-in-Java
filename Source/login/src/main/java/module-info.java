d9e38d9553819cbe84c4e104b1827f18e0fc23e1
thao-tnpt <tranthaohvc@gmail.com>
2026-04-09 20:57:03 +0700

    Thêm giao diện Login và thư mục Client

--- Source/login/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.login {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires java.sql;
-
-    opens com.example.login to javafx.fxml;
-    exports com.example.login;
-}
